46a76f562f7d49ec863b71dec44d0617c0c9b778
cdgraybill <cdgraybill@gmail.com>
2021-09-06 14:35:15 -0500

    Initial commit

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    Rigidbody rigidBody;
+    [SerializeField] float timeToWait = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        meshRenderer.enabled = false;
+        rigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeElapsed = Time.time;
+        
+        if (timeElapsed > timeToWait)
+        {
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Obstacle-Course
+ A 3rd person obstacle course created in Unity
